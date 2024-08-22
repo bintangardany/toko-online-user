2a9aca7cff8eb2b0dd8ea2103104850895d04af5
bintangardany <bintangjr731@gmail.com>
2024-08-22 15:14:35 +0700

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -12,9 +12,9 @@ const HomePage = async () => {
 
   return (
     <Container>
-     <div className="space-y-10 pb-10">
+      <div className="space-y-10 pb-10">
         <Banner data={banner} />
-        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8"> */}
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Produk Unggulan" items={products} />
         </div>
       </div>
