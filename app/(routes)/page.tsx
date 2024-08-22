cf68fd81e44704aab25bbfda67973553765c3343
bintangardany <bintangjr731@gmail.com>
2024-08-22 15:21:52 +0700

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,12 +8,9 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const banner = await getBanner("7b214efd-45d2-449a-a79c-41f27f0660f8");
 
   return (
     <Container>
-      <div className="space-y-10 pb-10">
-        <Banner data={banner} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Produk Unggulan" items={products} />
         </div>
